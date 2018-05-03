dae426299b91058c9fda6e29f74750db2364b6f8
DonnyStout <donnystout47@gmail.com>
2018-05-02 23:51:51 -0600

    added expense and expense type tables and accessors and mutators.

--- app/src/main/java/edu/cnm/deepdive/blackboardbudget/models/Expense.java
+++ app/src/main/java/edu/cnm/deepdive/blackboardbudget/models/Expense.java
@@ -1,5 +1,69 @@
 package edu.cnm.deepdive.blackboardbudget.models;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.ForeignKey;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity(foreignKeys = {@ForeignKey(entity = User.class,
+    parentColumns = "user_id",
+    childColumns = "user_id")})
 public class Expense {
 
+
+  @PrimaryKey
+  @ColumnInfo(name = "expense_id")
+  private long expenseId;
+
+  @ColumnInfo(name = "date")
+  private String date;
+
+  @ColumnInfo(name = "name")
+  private String name;
+
+  @ColumnInfo(name = "amount")
+  private long amount;
+
+  @ColumnInfo(name = "user_id")
+  private long userId;
+
+  public long getExpenseId() {
+    return expenseId;
+  }
+
+  public void setExpenseId(long expenseId) {
+    this.expenseId = expenseId;
+  }
+
+  public String getDate() {
+    return date;
+  }
+
+  public void setDate(String date) {
+    this.date = date;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public long getAmount() {
+    return amount;
+  }
+
+  public void setAmount(long amount) {
+    this.amount = amount;
+  }
+
+  public long getUserId() {
+    return userId;
+  }
+
+  public void setUserId(long userId) {
+    this.userId = userId;
+  }
 }
--- app/src/main/java/edu/cnm/deepdive/blackboardbudget/models/ExpenseType.java
+++ app/src/main/java/edu/cnm/deepdive/blackboardbudget/models/ExpenseType.java
@@ -1,5 +1,49 @@
 package edu.cnm.deepdive.blackboardbudget.models;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.ForeignKey;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity(foreignKeys = {@ForeignKey(entity = User.class,
+    parentColumns = "type_id",
+    childColumns = "type_id"), @ForeignKey(entity = Expense.class,
+    parentColumns = "expense_id",
+    childColumns = "expense_id")})
 public class ExpenseType {
 
+
+  @PrimaryKey
+  @ColumnInfo(name = "expensetype_id")
+  private long expenseTypeId;
+
+  @ColumnInfo(name = "expense_id")
+  private long expenseId;
+
+  @ColumnInfo(name = "type_id")
+  private long typeId;
+
+  public long getExpenseTypeId() {
+    return expenseTypeId;
+  }
+
+  public void setExpenseTypeId(long expenseTypeId) {
+    this.expenseTypeId = expenseTypeId;
+  }
+
+  public long getExpenseId() {
+    return expenseId;
+  }
+
+  public void setExpenseId(long expenseId) {
+    this.expenseId = expenseId;
+  }
+
+  public long getTypeId() {
+    return typeId;
+  }
+
+  public void setTypeId(long typeId) {
+    this.typeId = typeId;
+  }
 }
