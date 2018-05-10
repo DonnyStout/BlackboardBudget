cbc440a25716c0d563cdfb372be1333f20d8cab6
DonnyStout <donnystout47@gmail.com>
2018-05-10 17:53:07 -0600

    added query for budget type

--- app/src/main/java/edu/cnm/deepdive/blackboardbudget/dao/BudgetTypeDao.java
+++ app/src/main/java/edu/cnm/deepdive/blackboardbudget/dao/BudgetTypeDao.java
@@ -1,5 +1,13 @@
 package edu.cnm.deepdive.blackboardbudget.dao;
 
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Query;
+import edu.cnm.deepdive.blackboardbudget.models.BudgetType;
+
+@Dao
 public interface BudgetTypeDao {
+
+  @Query("SELECT * FROM budgettype WHERE budgettype_id LIKE :budgetTypeID LIMIT 1")
+  BudgetType findByBudgetTypeId(long budgetTypeId);
 
 }
