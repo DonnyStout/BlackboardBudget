d5256a7d3716502d77f9c8c40f67c7b24fcd99de
DonnyStout <donnystout47@gmail.com>
2018-05-10 18:10:08 -0600

    added queries for type table

--- app/src/main/java/edu/cnm/deepdive/blackboardbudget/dao/TypeDao.java
+++ app/src/main/java/edu/cnm/deepdive/blackboardbudget/dao/TypeDao.java
@@ -1,5 +1,19 @@
 package edu.cnm.deepdive.blackboardbudget.dao;
 
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Query;
+import edu.cnm.deepdive.blackboardbudget.models.Type;
+
+@Dao
 public interface TypeDao {
+
+  @Query("SELECT * FROM type")
+  Type getAll();
+
+  @Query("SELECT * FROM type WHERE type_id LIKE :typeId LIMIT 1")
+  Type findById(long typeId);
+
+  @Query("SELECT * FROM type WHERE type LIKE :type LIMIT 1")
+  Type findByType(String type);
 
 }
