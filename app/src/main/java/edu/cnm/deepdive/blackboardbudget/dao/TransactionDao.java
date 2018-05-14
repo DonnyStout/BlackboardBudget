6ee6235daa589630b2be176800a30057fc6ef0e1
DonnyStout <donnystout47@gmail.com>
2018-05-14 14:03:21 -0600

    added queries for transaction table

--- app/src/main/java/edu/cnm/deepdive/blackboardbudget/dao/TransactionDao.java
+++ app/src/main/java/edu/cnm/deepdive/blackboardbudget/dao/TransactionDao.java
@@ -1,5 +1,21 @@
 package edu.cnm.deepdive.blackboardbudget.dao;
 
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Query;
+import edu.cnm.deepdive.blackboardbudget.models.Transaction;
+import java.util.List;
+import org.joda.time.LocalDate;
+
+@Dao
 public interface TransactionDao {
+
+  @Query("SELECt * FROM `transaction`")
+  List<Transaction> getAll();
+
+  @Query("SELECT * FROM `transaction` WHERE date LIKE :date AND user_id LIKE :userId LIMIT 1")
+  List<Transaction> findByDateAndUserId(LocalDate date, long userId);
+
+  @Query("SELECT * FROM `transaction` WHERE name LIKE :name AND user_id LIKE :userId")
+  Transaction findByNameAndUserId(String name, long userId);
 
 }
