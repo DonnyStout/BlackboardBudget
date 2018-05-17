95f2943db850bbe2af44633204988e8282a6e0d7
DonnyStout <donnystout47@gmail.com>
2018-05-16 18:58:15 -0600

    added queries for transactiontype table

--- app/src/main/java/edu/cnm/deepdive/blackboardbudget/dao/TransactionTypeDao.java
+++ app/src/main/java/edu/cnm/deepdive/blackboardbudget/dao/TransactionTypeDao.java
@@ -1,5 +1,16 @@
 package edu.cnm.deepdive.blackboardbudget.dao;
 
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Query;
+import edu.cnm.deepdive.blackboardbudget.models.TransactionType;
+
+@Dao
 public interface TransactionTypeDao {
 
+
+  @Query("SELECT * FROM transactiontype WHERE transaction_id LIKE :transactionId LIMIT 1")
+  TransactionType findByTransactionId(long transactionId);
+
+  @Query("SELECT * FROM transactiontype WHERE type_id LIKE :typeId LIMIT 1")
+  TransactionType findByTypeId(long typeId);
 }
