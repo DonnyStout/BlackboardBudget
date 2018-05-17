b1911958251b25d585968b8ef9a88f3331e9cfba
DonnyStout <donnystout47@gmail.com>
2018-05-16 19:00:25 -0600

    added queries for expensetype table

--- app/src/main/java/edu/cnm/deepdive/blackboardbudget/dao/ExpenseTypeDao.java
+++ app/src/main/java/edu/cnm/deepdive/blackboardbudget/dao/ExpenseTypeDao.java
@@ -1,5 +1,16 @@
 package edu.cnm.deepdive.blackboardbudget.dao;
 
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Query;
+import edu.cnm.deepdive.blackboardbudget.models.ExpenseType;
+
+@Dao
 public interface ExpenseTypeDao {
 
+
+  @Query("SELECT * FROM expensetype WHERE expense_id LIKE :expenseId LIMIT 1")
+  ExpenseType findByExpenseId(long expenseId);
+
+  @Query("SELECT * FROM expensetype WHERE type_id LIKE :typeId LIMIT 1")
+  ExpenseType findByTypeId(long typeId);
 }
